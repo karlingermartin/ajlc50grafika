087d2f2bd9fcd0f196d77bb193058e6e0118ab4d
karlingermartin <kmartin990708@gmail.com>
2021-03-04 02:20:15 +0100

    gyak_01, gyak_02

---
+++ matrices/matrix.h
@@ -0,0 +1,27 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+/**
+ * Initializes all elements of the matrix to zero.
+ */
+void init_zero_matrix(float matrix[3][3]);
+
+
+//init identity
+void init_identity_matrix(float matrix[3][3]);
+
+
+void mult_scalar_matrix(float matrix[3][3], float scalar);
+
+/**
+ * Print the elements of the matrix.
+ */
+void print_matrix(const float matrix[3][3]);
+
+/**
+ * Add matrices.
+ */
+void add_matrices(const float a[3][3], const float b[3][3], float c[3][3]);
+
+#endif // MATRIX_H
+
